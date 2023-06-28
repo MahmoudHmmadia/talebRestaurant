b7f0386de392acd1253428cbcaaa8a325a81b336
Mahmoud Hmmadia <102804429+MahmoudHmmadia@users.noreply.github.com>
2023-06-28 13:13:51 +0300

    fixing

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const URL = "https://www.taleb_restaurant_api.onrender.com/";
+const URL = "https://taleb-restaurant-api.onrender.com/";
 const myAxios = axios.create({
   baseURL: URL,
   headers: {
