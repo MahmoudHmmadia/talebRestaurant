a6d91b938715608c227671549ca65d39f3494b94
Mahmoud Hmmadia <102804429+MahmoudHmmadia@users.noreply.github.com>
2023-07-15 15:27:44 +0300

    fix styling

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 const URL = "https://taleb-restaurant-api.onrender.com/";
-const localUrl = "http://localhost:3500/";
+const LOCAL_URL = "http://localhost:3500/";
 const myAxios = axios.create({
-  baseURL: localUrl,
+  baseURL: URL,
   headers: {
     "Content-Type": "Application/json",
   },
